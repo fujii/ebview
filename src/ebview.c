aa3002c6f3b9fd49280bcda0fe9c8810a871f2bc
Fujii Hironori <fujii.hironori@gmail.com>
2008-11-29 17:55:04 +0900

    * src/ebview.c: Fix compile error. Do not define GTK_DISABLE_DEPRECATED.

--- src/ebview.c
+++ src/ebview.c
@@ -15,7 +15,7 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
-#define GTK_DISABLE_DEPRECATED 1
+//#define GTK_DISABLE_DEPRECATED 1
 
 #define _GLOBAL
 #include <signal.h>
