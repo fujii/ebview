abbfbee24d271ce274aeeaf1c644717e302a95a5
Fujii Hironori <fujii.hironori@gmail.com>
2012-08-17 00:25:23 +0900

    pango_x is deprecated. Use pango_xft instead.

--- src/preference.c
+++ src/preference.c
@@ -27,7 +27,7 @@
 #ifdef __WIN32__
 #include <pango/pangowin32.h>
 #else
-#include <pango/pangox.h>
+#include <pango/pangoxft.h>
 #endif
 
 #include "dictbar.h"
@@ -429,7 +429,7 @@ void  calculate_font_size(){
 		return;
 	}
 
-	fontmap = pango_x_font_map_for_display(display);
+	fontmap = pango_xft_get_font_map(display, 0);
 #endif
 
 	if(fontmap == NULL){
